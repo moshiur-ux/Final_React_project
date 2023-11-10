386b794394ba487056827f9a8aede0877444b616
moshiur <55761320+moshiur-ux@users.noreply.github.com>
2023-11-10 21:49:15 +0600

    adding components section

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,22 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+
+
+const PopularMenu = () => {
+    return (
+        <div>
+            <section>
+                <SectionTitle
+
+                heading="From Our Menu"
+                subHeading="Popular Items"
+                
+                >
+                    
+                </SectionTitle>
+            </section>
+            
+        </div>
+    );
+};
+
+export default PopularMenu;
